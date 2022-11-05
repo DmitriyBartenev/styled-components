345a8dd78b70500b2c00aa98a4ff0bfb2798c32d
DmitriyBartenev <bartenevdmitriy@bk.ru>
2022-11-05 16:59:26 +0400

    styled components commit

---
+++ src/App/App.js
@@ -0,0 +1,13 @@
+import { Component } from "react";
+import CounterComponent from "../Components/Counter";
+
+class App extends Component{
+
+    render(){
+        return(
+            <CounterComponent/>
+        )
+    }
+}
+
+export default App;
