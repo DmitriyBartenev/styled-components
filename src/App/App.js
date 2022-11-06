2ee2c1d4920c7962e318e6de232fbefed01c69f6
DmitriyBartenev <bartenevdmitriy@bk.ru>
2022-11-06 23:39:39 +0400

    css-in-js commit

--- src/App/App.js
+++ src/App/App.js
@@ -1,11 +1,39 @@
 import { Component } from "react";
-import CounterComponent from "../Components/Counter";
+import Header from '../Components/Header';
+import Footer from "../Components/Footer";
+import Card from "../Components/Card";
+import { ThemeProvider } from "styled-components";
+import GlobalStyles from "../Components/styles/Global";
+import content from '../content';
+import { Container } from "../Components/styles/Container.styled";
+
+const theme = {
+    colors:{
+        header: '#ebfbff',
+        body:'#fff',
+        footer:'#003333',
+    },
+    mobile:'768px',    
+}
 
 class App extends Component{
 
     render(){
         return(
-            <CounterComponent/>
+            <ThemeProvider theme={theme}>
+                <>
+                    <GlobalStyles/>
+                    <Header/>
+                    <Container>
+                        {content.map((item, id) => {
+                            return(
+                                <Card key={id} item={item}/>
+                            )
+                        })}
+                    </Container>
+                    <Footer/>
+                </>
+            </ThemeProvider>
         )
     }
 }
